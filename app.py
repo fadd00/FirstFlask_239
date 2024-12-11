fc1fe85dbc56e2058d6d7945a35dbeb60bd83d79
fadd00 <andhikahutama9@gmail.com>
2024-12-11 16:39:46 +0700

    sdfsg

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here' 
+app.secret_key = 'wleleo' 
 
 USERS = {
     'admin': 'password123',
