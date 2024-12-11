9b817213d76483145146d9e05f590737b79259e6
fadd00 <andhikahutama9@gmail.com>
2024-12-11 15:07:04 +0700

    `Removed index.html template file`

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'  # Penting untuk flash messages
+app.secret_key = 'your_secret_key_here' 
 
-# Contoh data pengguna (biasanya akan tersimpan di database)
 USERS = {
     'admin': 'password123',
     'user': 'user123'
